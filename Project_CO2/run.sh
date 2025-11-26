f750f4561ad28b2eb7dd0af00dcf2bf868970e23
lhuynh35 <128656561+Fenteay@users.noreply.github.com>
2025-11-26 18:45:21 +0700

    update run.sh

--- Project_CO2/run.sh
+++ Project_CO2/run.sh
@@ -2,13 +2,10 @@
 
 # Chạy FastAPI
 # python -m uvicorn main:app --host 0.0.0.0 --port 8000 --reload &
-python -m uvicorn main:app --port 8000 --reload &
-
-# Chạy Streamlit App
-python -m streamlit run app.py
+python -m uvicorn api_forecast:app --port 8000 --reload 
 
 # Chạy Panel Dashboard
 # python -m panel serve dashboard.py --address 0.0.0.0 --port 5006 --autoreload --show
-python -m panel serve dashboard.py --port 5006 --autoreload --show
+python -m panel serve main_app.py --port 5006 --autoreload --show
 
-python -m panel serve dashboard.py --address 0.0.0.0 --port 5006 --allow-websocket-origin=192.168.1.245:5006 --allow-websocket-origin="*" --autoreload
+python -m panel serve main_app.py --address 0.0.0.0 --port 5006 --allow-websocket-origin={ip-host}:5006 --allow-websocket-origin="*" --autoreload
--- Template web (FastAPI + Panel)/run.sh
+++ Template web (FastAPI + Panel)/run.sh
@@ -2,8 +2,10 @@
 
 # Chạy FastAPI
 # python -m uvicorn main:app --host 0.0.0.0 --port 8000 --reload &
-python -m uvicorn main:app --port 8000 --reload &
+python -m uvicorn api_forecast:app --port 8000 --reload 
 
 # Chạy Panel Dashboard
 # python -m panel serve dashboard.py --address 0.0.0.0 --port 5006 --autoreload --show
-python -m panel serve dashboard.py --port 5006 --autoreload --show
+python -m panel serve main_app.py --port 5006 --autoreload --show
+
+python -m panel serve main_app.py --address 0.0.0.0 --port 5006 --allow-websocket-origin={ip-host}:5006 --allow-websocket-origin="*" --autoreload
